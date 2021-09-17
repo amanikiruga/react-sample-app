14becc357e882d2635839321cc20767428a125d0
Amani Kiruga <amaniedu71@gmail.com>
2021-09-17 05:13:39 -0400

    changed state logic, started time travel logic

--- src/index.js
+++ src/index.js
@@ -44,18 +44,9 @@ class Board extends React.Component {
         );
     }
 
-    
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-
-        if (winner) status = "WINNER: " + winner;
-        else
-            status = status =
-                "Next player: " + (this.state.xIsNext ? "X" : "O");
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -84,15 +75,56 @@ class Game extends React.Component {
             xIsNext: true,
         };
     }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if (squares[i] || calculateWinner(squares)) return;
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        //Square components re-render auto and Board has full control
+        // over square components known as controlled com=ponenets.
+        this.setState({
+            history: history.concat([{ squares: squares }]),
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? "Go to game # " + move : "Go to game start";
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}> {desc} </button>
+                </li>
+            );
+        });
+
+        let status;
+
+        if (winner) status = "WINNER: " + winner;
+        else
+            status = status =
+                "Next player: " + (this.state.xIsNext ? "X" : "O");
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board squares={} />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => {
+                            this.handleClick(i);
+                        }}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
